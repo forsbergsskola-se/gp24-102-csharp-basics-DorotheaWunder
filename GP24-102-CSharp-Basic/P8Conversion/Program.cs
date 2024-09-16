078e33cb437c2e5bacc07649af59cce91ccf04db
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-16 12:45:59 +0200

    Test

---
+++ GP24-102-CSharp-Basic/P8Conversion/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Give me a number");
+
+var inputNr = double. Parse(Console. ReadLine());
+Console.WriteLine(inputNr);
+
+//for whatever reason the number is rounded up if nr behind comma is > 5
+int integerNr = Convert.ToInt32(inputNr);
+Console.WriteLine(integerNr);
