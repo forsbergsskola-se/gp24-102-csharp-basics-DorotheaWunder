0b09235981020ca8795bc1debe2c660bd00253f6
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-16 15:43:16 +0200

    P10Boolean
    redone the whole assignment again

--- GP24-102-CSharp-Basic/P10Boolean/Program.cs
+++ GP24-102-CSharp-Basic/P10Boolean/Program.cs
@@ -1,18 +1,18 @@
-﻿int age;
+﻿int age = 0;
+bool isChild;
+bool isTeenager;
+bool isAdult;
 
 Console.WriteLine("What is your age?");
-string inputStringAge = Console.ReadLine();
-int nrAge = Convert.ToInt32(inputStringAge);
-age =  +nrAge;
+int ageInput = Convert.ToInt32(Console.ReadLine());
+age = +ageInput;
 
-Console.WriteLine(nrAge);
+Console.WriteLine(age);
 
-bool isChild = age <= 12;
-//bool isTeenager = age <= 13 || age >= 19;
-bool isTeenager = !(age <12 || age >= 20);
-bool isAdult = age > 19;
+isChild = age <= 12;
+isTeenager = age > 12 && age <= 19;
+isAdult = age > 19;
 
-Console.WriteLine("You are a child: " + isChild);
-Console.WriteLine("You are a teenager: " + isTeenager);
-Console.WriteLine("You are an adult: " + isAdult);
-
+Console.WriteLine("You are a child " + isChild);
+Console.WriteLine("You are a teenager " + isTeenager);
+Console.WriteLine("You are an adult " + isAdult);
