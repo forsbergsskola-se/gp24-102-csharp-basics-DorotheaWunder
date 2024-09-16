b1b2166929d6f3d5d9358ae6c0bb3f510dffb976
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-16 14:31:15 +0200

    P10Boolean
    determining if a number is larger, smaller or inbetween a certain value

---
+++ GP24-102-CSharp-Basic/P10Boolean/Program.cs
@@ -0,0 +1,18 @@
+﻿int age;
+
+Console.WriteLine("What is your age?");
+string inputStringAge = Console.ReadLine();
+int nrAge = Convert.ToInt32(inputStringAge);
+age =  +nrAge;
+
+Console.WriteLine(nrAge);
+
+bool isChild = age <= 12;
+//bool isTeenager = age <= 13 || age >= 19;
+bool isTeenager = !(age <12 || age >= 20);
+bool isAdult = age > 19;
+
+Console.WriteLine("You are a child: " + isChild);
+Console.WriteLine("You are a teenager: " + isTeenager);
+Console.WriteLine("You are an adult: " + isAdult);
+
