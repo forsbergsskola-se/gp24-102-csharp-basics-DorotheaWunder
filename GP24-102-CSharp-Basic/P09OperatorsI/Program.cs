69414f7f28911fe8a19dfbd208564e15ea23301c
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-17 15:22:40 +0200

    P09OperatorsI
    P09_08BMI task done

---
+++ GP24-102-CSharp-Basic/P09OperatorsI/Program.cs
@@ -0,0 +1,13 @@
+﻿float valueWeight;
+float valueHeight;
+float valueBmi;
+
+Console.WriteLine("Enter your weight in kg:");
+valueWeight = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your height in m:");
+valueHeight = float.Parse(Console.ReadLine());
+
+valueBmi =  valueWeight / (float)Math.Pow(valueHeight, 2);
+
+Console.WriteLine($"A person with a weight of {valueWeight} and a height of {valueHeight} has a BMI of {valueBmi}");
