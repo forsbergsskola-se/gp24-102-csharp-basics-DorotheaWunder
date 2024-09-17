b1c012143e1ce975aaacfc634b07239c15d393df
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-17 10:43:44 +0200

    P11IfElseD
    completed P11_3 - vowel or consonant

---
+++ GP24-102-CSharp-Basic/P11IfElseD/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter a letter");
+string inputLetter = Console.ReadLine();
+
+string typeLetter;
+//I'm pretty sure there is a more elegant and less cluttered solution -.-
+if (inputLetter == "a" || inputLetter == "e" || inputLetter == "i" || inputLetter == "o" || inputLetter == "u")
+{
+    typeLetter = "vowel";
+}
+else
+{
+    typeLetter = "consonant";
+}
+
+Console.WriteLine("Your letter is " + inputLetter + " so your letter is a " + typeLetter);
