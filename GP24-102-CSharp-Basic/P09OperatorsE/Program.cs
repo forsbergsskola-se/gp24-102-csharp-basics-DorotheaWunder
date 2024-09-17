e3a291d2a510c31a95b60754a36e6abfacba5c46
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-17 14:21:29 +0200

    P09OperatorsE
    P09_04Remainder task done

---
+++ GP24-102-CSharp-Basic/P09OperatorsE/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA;
+int numberB;
+int modulo;
+
+Console.WriteLine("Your first number is: ");
+numberA = Convert.ToInt32(Console.ReadLine());
+    
+Console.WriteLine("Your second number is: ");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+modulo = numberA % numberB;
+
+Console.WriteLine($"The remainder of {numberA} and {numberB} is: {modulo}");
