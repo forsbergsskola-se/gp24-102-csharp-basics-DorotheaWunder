7049fbb89593e0e7784f2c434e78f6af95b33c89
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-17 14:13:20 +0200

    P09OperatorsD
    P09_03Division task done

---
+++ GP24-102-CSharp-Basic/P09OperatorsC/Program.cs
@@ -0,0 +1,9 @@
+﻿int minutes;
+int seconds;
+
+Console.WriteLine("How many minutes do you have?");
+minutes = Convert.ToInt32(Console.ReadLine());
+
+seconds = minutes * 60;
+
+Console.WriteLine($"{minutes} minutes? That's {seconds} seconds.");
---
+++ GP24-102-CSharp-Basic/P09OperatorsD/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA;
+int numberB;
+float result;
+
+Console.WriteLine("Your first number is:");
+numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your second number is:");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+float numberAfloat = (float)numberA;
+float numberBfloat = (float)numberB;
+result = numberAfloat / numberBfloat;
+
+Console.WriteLine($"{numberA} divided by {numberB} = {result}");
