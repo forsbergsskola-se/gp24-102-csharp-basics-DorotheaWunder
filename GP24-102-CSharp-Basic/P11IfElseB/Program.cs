071a0cc463d541edeb2608f99f0c3ec8e405d783
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-17 11:54:35 +0200

    P11IfElseB
    P11_1Grades task completed

--- GP24-102-CSharp-Basic/P11IfElseB/Program.cs
+++ GP24-102-CSharp-Basic/P11IfElseB/Program.cs
@@ -1,9 +1,33 @@
 ﻿int pointsGrade;
 string letterGrade;
 
-Console.WriteLine("How many points did you get in the test?");
+Console.WriteLine("How many points from 0 - 100 did you get in the test?");
+pointsGrade = Convert.ToInt32(Console.ReadLine());
 
-
+if (pointsGrade >= 90 && pointsGrade <= 100)
+{
+    letterGrade = "A";
+}
+else if (pointsGrade >= 80 && pointsGrade <= 89)
+{
+    letterGrade = "B";
+}
+else if (pointsGrade >= 70 && pointsGrade <= 79)
+{
+    letterGrade = "C";
+}
+else if (pointsGrade >= 60 && pointsGrade <= 69)
+{
+    letterGrade = "D";
+}
+else if (pointsGrade < 60)
+{
+    letterGrade = "F";
+}
+else
+{
+    letterGrade = "an F- because you don't know how to enter a correct number or you cheated!";
+}
 
 
 
