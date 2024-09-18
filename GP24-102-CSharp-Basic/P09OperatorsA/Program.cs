f1bed6b86d26e7e9aa24cd625e0608def493ddad
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-18 10:47:17 +0200

    P09OperatorsA
    Final exercise task done

--- GP24-102-CSharp-Basic/P09OperatorsA/Program.cs
+++ GP24-102-CSharp-Basic/P09OperatorsA/Program.cs
@@ -1,4 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int inputSeconds;
+int calculatedSeconds;
+int calculatedMinutes;
+int calculatedHours;
+int calculatedDays;
+float calculatedDaysFloat;
 
-Console.WriteLine("Hello, World!");
-//slides recommended to do this later in the lecture
+Console.WriteLine("Give me a number of seconds");
+inputSeconds = Convert.ToInt32(Console.ReadLine());
+
+calculatedDaysFloat = (float)inputSeconds / 86400;
+calculatedSeconds = inputSeconds / 60;
+calculatedMinutes = calculatedSeconds / 60;
+calculatedHours = calculatedMinutes / 24;
+calculatedDays = calculatedHours / 365;
+
+calculatedDays = calculatedHours % 365;
+calculatedHours = calculatedMinutes % 24;
+calculatedMinutes = calculatedSeconds % 60;
+calculatedSeconds = inputSeconds % 60;
+
+Console.WriteLine($"{calculatedSeconds} seconds");
+Console.WriteLine($"{calculatedMinutes} minutes");
+Console.WriteLine($"{calculatedHours} hours");
+Console.WriteLine($"{calculatedDays} days");
+
+Console.WriteLine($"{calculatedDays} - {calculatedHours} - {calculatedMinutes} - {calculatedSeconds}");
+Console.WriteLine($"In total, that's {calculatedDaysFloat} days");
