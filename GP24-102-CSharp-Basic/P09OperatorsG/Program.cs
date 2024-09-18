064e52722198e63d9d5b94e4156972b985a8ac67
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-18 10:57:49 +0200

    P09OperatorsG
    P09_06Negation task done

--- GP24-102-CSharp-Basic/P09OperatorsG/Program.cs
+++ GP24-102-CSharp-Basic/P09OperatorsG/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int numberInput;
+int numberNegative;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Write a number");
+numberInput = Convert.ToInt32(Console.ReadLine());
+
+numberNegative = -numberInput;
+
+Console.WriteLine($"The negative of {numberInput} is {numberNegative}");
