a2010e003784ceebeeabaf02a4c46375ef4f3dc2
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-18 12:59:14 +0200

    P12Random
    P12_1Random_Coordinates task done
    P12_2Random_Password task done

---
+++ GP24-102-CSharp-Basic/P12RandomB/Program.cs
@@ -0,0 +1,5 @@
+﻿Random random = new Random();
+int randomX = random.Next(1,101);
+int randomY = random.Next(1,101);;
+
+Console.WriteLine($"The enemy is at X: {randomX} and Y: {randomY}");
---
+++ GP24-102-CSharp-Basic/P12RandomE/Program.cs
@@ -0,0 +1,8 @@
+﻿Random random = new Random();
+string messageSuccess = "Wer das liest is doof.";
+string messageFailure = "Try again.";
+double chance = 0.0;
+double randomRoll = 0.0;
+
+
+Console.WriteLine("Hello, World!");
