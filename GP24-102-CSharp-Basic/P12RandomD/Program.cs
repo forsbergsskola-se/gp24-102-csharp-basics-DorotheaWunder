091b5fc7e026019dc2fd0ccafaa2e042430136db
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-18 13:18:13 +0200

    P12RandomD
    P12_3Random_Item task done

--- GP24-102-CSharp-Basic/P12RandomD/Program.cs
+++ GP24-102-CSharp-Basic/P12RandomD/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Random random = new Random();
+string[] itemList;
+int listPosition;
+string itemChoice;
 
-Console.WriteLine("Hello, World!");
+itemList = new string [] { "Sword", "Bow", "Spear", "Club", "Axe" };
+listPosition = random.Next(itemList.Length);
+itemChoice = itemList[listPosition];
+
+Console.WriteLine($"Congratulations, you got: {itemChoice}");
