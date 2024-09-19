8414f6914f21f45328a89f39d0d7161254403497
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-19 11:48:35 +0200

    P13GoTo
    P13_1Dollars task done
    P13_2Triangle task done

--- GP24-102-CSharp-Basic/P13GoToB/Program.cs
+++ GP24-102-CSharp-Basic/P13GoToB/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int inputDollarNumber;
+string outputDollarNumber;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Scrooge McDuck Bank. How many Dollars would you like to retract?");
+inputDollarNumber = Convert.ToInt32(Console.ReadLine());
+
+outputDollarNumber = new string('$', inputDollarNumber);
+Console.WriteLine($"Here's your money: {outputDollarNumber}");
--- GP24-102-CSharp-Basic/P13GoToC/Program.cs
+++ GP24-102-CSharp-Basic/P13GoToC/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int inputNumberRows;
+string outputAsciiSign;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("How many rows is your triangle suppose to have?");
+inputNumberRows = Convert.ToInt32(Console.ReadLine());
+
+PrintAscii:
+outputAsciiSign = new string('#', inputNumberRows);
+Console.WriteLine(outputAsciiSign);
+
+if (inputNumberRows != 0)
+{
+    inputNumberRows = inputNumberRows - 1;
+    goto PrintAscii;
+}
