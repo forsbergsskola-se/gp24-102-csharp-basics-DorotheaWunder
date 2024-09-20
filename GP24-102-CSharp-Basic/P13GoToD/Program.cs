be8efec9948b7293539dcfa4409567afb93b71c8
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-20 09:36:09 +0200

    P13GoTo
    P13GoTo main task done

--- GP24-102-CSharp-Basic/P13GoToD/Program.cs
+++ GP24-102-CSharp-Basic/P13GoToD/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int inputNumber;
+char charAssigned;
+string outputAsciiSign;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("How many rows do you want for your pattern?");
+inputNumber = Convert.ToInt32(Console.ReadLine());
+
+//int modulo = inputNumber % 2;
+//charAssigned = modulo != 0 ? "#" : "-";
+
+for (int i = 0; i < inputNumber; i++ )
+{
+    for (int j = 0; j < inputNumber; j++ )
+    {
+        outputAsciiSign = new string('#', inputNumber);
+        Console.WriteLine(outputAsciiSign);
+    }
+    outputAsciiSign = new string('-', inputNumber);
+    Console.WriteLine(outputAsciiSign);
+    Console.WriteLine("\n");
+}
+
+
+
+//nested loop; outer for #, inner for -
+//inner and out loop both use same input nr
+//where does goto enter??? (bottom of outer loop?
+//will I need row and column?
+//ist inputnr gerade? # Inputnr ungerqde -
