f9d6cdf458cd80b5c47086b0b798dff2923614dc
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-23 12:07:56 +0200

    Nim Tic-Tac-Toe
    Managed to do a colored 3x3 grid. yay

---
+++ GP24-102-CSharp-Basic/NimB/Program.cs
@@ -0,0 +1,35 @@
+﻿int row;
+int column;
+string[,] grid = new string[3, 3]
+  {
+    { " ", " ", " " },
+    { " ", " ", " " },
+    { " ", " ", " " }
+  };
+
+
+ConsoleColor AssignColorSquares(int row, int column)
+ {
+  return (row + column) % 2 == 0 ? ConsoleColor.Yellow : ConsoleColor.DarkYellow;
+ }
+
+
+void DisplayGrid()
+ {
+   for(int i = 0; i < grid.GetLength(0); i++)
+   {
+    for (int j = 0; j < grid.GetLength(1); j++)
+    {
+      Console.BackgroundColor = AssignColorSquares(i, j);
+      Console.ForegroundColor = Console.BackgroundColor == ConsoleColor.Yellow
+       ? ConsoleColor.DarkYellow
+       : ConsoleColor.Yellow;
+
+      Console.Write(" " + grid[i,j] + " ");
+      Console.ResetColor();
+    }
+    Console.WriteLine();
+   }
+ }
+ 
+DisplayGrid();
