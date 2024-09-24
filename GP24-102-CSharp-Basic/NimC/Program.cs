83333aef5609d1958a2e5e154bc2df2993fcf914
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-09-24 14:29:25 +0200

    Battleships
    - 10x10 grid with different colors for each player
    -array of different ship types

---
+++ GP24-102-CSharp-Basic/NimC/Program.cs
@@ -0,0 +1,96 @@
+﻿bool isPlayerGreen = true;
+
+string[] shipTypes =
+{
+    "2 SQUARES",
+    "2 SQUARES",
+    "3 SQUARES",
+    "4 SQUARES",
+    "5 SQUARES",
+};
+string[,] gridGreen = 
+{
+    { " ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", " "},
+    { "1", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "1"},
+    { "2", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "2"},
+    { "3", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "3"},
+    { "4", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "4"},
+    { "5", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "5"},
+    { "6", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "6"},
+    { "7", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "7"},
+    { "8", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "8"},
+    { "9", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "9"},
+    { "0", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "0"},
+    { " ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", " "},
+};
+string[,] gridPink = 
+{
+    { " ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", " "},
+    { "1", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "1"},
+    { "2", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "2"},
+    { "3", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "3"},
+    { "4", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "4"},
+    { "5", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "5"},
+    { "6", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "6"},
+    { "7", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "7"},
+    { "8", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "8"},
+    { "9", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "9"},
+    { "0", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "0"},
+    { " ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", " "},
+};
+
+DisplayGrid();
+
+
+
+
+
+
+
+ConsoleColor AssignColorSquares(int row, int column)
+{
+    if (isPlayerGreen)
+    {
+            return (row + column) % 2 == 0 ? ConsoleColor.Green : ConsoleColor.DarkGreen;
+    }
+    else
+    {
+        return (row + column) % 2 == 0 ? ConsoleColor.Magenta : ConsoleColor.DarkMagenta;
+    }
+}
+
+void DisplayGrid()
+{
+    if (isPlayerGreen)
+    {
+        for(int i = 0; i < gridGreen.GetLength(0); i++)
+        {
+            for (int j = 0; j < gridGreen.GetLength(1); j++)
+            {
+                Console.BackgroundColor = AssignColorSquares(i, j);
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.Write(" " + gridGreen[i,j] + " ");
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+        }
+    }
+    else
+    {
+        for(int i = 0; i < gridPink.GetLength(0); i++)
+        {
+            for (int j = 0; j < gridPink.GetLength(1); j++)
+            {
+                Console.BackgroundColor = AssignColorSquares(i, j);
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.Write(" " + gridPink[i,j] + " ");
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+        }
+    }
+
+    
+}
